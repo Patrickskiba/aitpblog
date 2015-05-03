848caa66d4b12783299283fb00e3ee8b4439b8a2
Patrick Skiba <skibap@my.easternct.edu>
2015-05-02 22:17:45 -0400

    With more styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  
+  root 'static_pages#home'
   get 'static_pages/constitution'
-
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
