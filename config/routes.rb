94c4f6df7155ae0d6895a19f00be0cffcf073de9
Patrick Skiba <skibap@my.easternct.edu>
2015-11-05 20:34:38 -0500

    Added members, changed video location, compressed pictures

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/constitution'
   get 'static_pages/events'
+  get 'static_pages/videos'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
