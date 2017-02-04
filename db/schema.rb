fa6d11c1d8cb25c3a2df1e0afeee714509953510
miguelito20 <lauerboy1@gmail.com>
2017-02-04 01:00:32 +0000

    updating eboard but not really because they wont give me pictures

--- db/schema.rb
+++ db/schema.rb
@@ -13,25 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151205170532) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "donations", force: :cascade do |t|
-    t.string   "company"
-    t.string   "reason"
-    t.string   "currentTech"
-    t.string   "pointOfContact"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "requests", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
